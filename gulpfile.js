e58597b373d54ba699cef1bb146d5c9b6fe29908
sergei-koltsov <serhiikoltsov@icloud.com>
2022-03-01 22:14:07 +0200

    first-page

--- gulpfile.js
+++ gulpfile.js
@@ -39,7 +39,7 @@ function styles() {
 function scripts() {
     return src([
             'node_modules/jquery/dist/jquery.js',
-            'node_modules/rateyo/src/jquery.rateyo.js',
+            'node_modules/swiper/swiper-bundle.min.js',
             'app/js/main.js'
         ])
         .pipe(concat('main.min.js'))
